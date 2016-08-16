4a840196b06d75302f24bc3d31a40bba0bd0239f
houquan <houquan@zuoyebang.com>
2016-08-16 14:24:41 +0800

    vector添加元素和元素访问

--- c20160812/c20160812/vector.cpp
+++ c20160812/c20160812/vector.cpp
@@ -15,4 +15,25 @@ int main() {
 	vector<string> vcstr1{"a", "b", "c"}; // 定义并初始化vector对象中的每个元素的值为"a", "b", "c", 这种写法也等价于
 	
 	vector<string> vcstr2 = { "a", "b", "c" };
+
+	for (int i = 0; i < 100; i ++) {
+		vcint.push_back(i);  // 通过push_back类成员函数来为空vector添加元素
+	}
+
+	vcint[9] = 89; // 可以通过索引来访问vector中的元素，索引访问返回的是元素的引用，所以可以重新赋值
+	// Note: 不可以通过索引为空的vector对象添加元素
+	cout << "vcint 中的第10个元素的值为：" << vcint[9] << endl;
+
+	// vector 的其他操作
+	// 检测是否为空
+	cout << "vcint 是否为空：" << vcint.empty() << endl;
+
+	// 检测vector的大小
+	cout << "vcint 的元素个数：" << vcint.size() << endl;
+
+	// 弹出vcint中的尾元素
+	vcint.pop_back();
+	cout << "弹出一个元素后vcint的大小：" << vcint.size() << "最后一个元素是：" << *(vcint.end() - 1) << endl;
+
+	getchar();
 }
